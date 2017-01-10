ea75cbcda34d46f98779de5f2b0981371aa29672
Alex-Joseph <alex.cheng@ryerson.ca>
2017-01-10 23:09:43 +0000

    Objects and functions

---
+++ function-arguements.js
@@ -0,0 +1,4 @@
+function math(fred, bob, joe) {
+  return bob * joe + fred;
+}
+console.log(math(53, 61, 67));
---
+++ if-statement.js
@@ -0,0 +1,6 @@
+var fruit = 'orange';
+var a = fruit.length;
+if (a > 5) { console.log("The fruit name has more than five characters.");
+} else {
+  console.log("The fruit name has five characters or less.");
+}
